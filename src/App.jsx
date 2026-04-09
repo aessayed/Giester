ba7f1ceceafa713fd986f8bba392e9c592c8511f
aessayed <ahmad.aessayed@gmail.com>
2026-04-09 10:45:05 +0200

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './index.css'
-import { LangProvider } from './i18n/Langcontext'
+import { LangProvider } from './i18n/LangContext'
 import Navbar   from './components/Navbar'
 import Hero     from './components/Hero'
 import Angebote from './components/Angebote'
